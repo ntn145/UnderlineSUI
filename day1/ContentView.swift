4567832e6a762020cca662757d6fe3baedecb868
trongnhaniot <trongnhaniot@gmail.com>
2022-08-02 20:21:19 +0700

    Initial Commit

---
+++ day1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  day1
+//
+//  Created by Khoai Alpha on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Developer")
+            .font(.largeTitle)
+            .underline()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day1/day1App.swift
@@ -0,0 +1,17 @@
+//
+//  day1App.swift
+//  day1
+//
+//  Created by Khoai Alpha on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
